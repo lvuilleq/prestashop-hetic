9d806c3c9a237e1fb0b50dcecb71081320ea38c5
lvuilleq <vuillequez.laura@gmail.com>
2015-04-01 11:45:55 +0200

    initial commit

---
+++ config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'presta_bbha');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', '');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'aMb8sV25JDc5FSvY6m5RoeIYMkDrV9Z7IVA09pA1Vcy0tLmRxHKVfTuT');
+define('_COOKIE_IV_', 'UobbH8GS');
+define('_PS_CREATION_DATE_', '2015-03-26');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.0.13');
+define('_RIJNDAEL_KEY_', 'a2cQr6J72JFz1sWI7b2gWA2I7Y5KOLwi');
+define('_RIJNDAEL_IV_', 'FrXKGqKSVh64L+/0v5O/Zw==');
